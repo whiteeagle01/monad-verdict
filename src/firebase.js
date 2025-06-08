e9e0bf3a453ad00ee8408ea02ea9344f6fd26de2
whiteeagle01 <turkozumustafa@gmail.com>
2025-06-08 12:09:10 +0300

    init

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app'; 
 import { getAuth, signInAnonymously } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBBtObCWj794A0RSDTq7ztLLilfDSflV-k",
   authDomain: "monad-verdict-app.firebaseapp.com",
   projectId: "monad-verdict-app",
-  storageBucket: "monad-verdict-app.firebasestorage.app",
+  storageBucket: "monad-verdict-app.appspot.com", // ✅ düzeltildi
   messagingSenderId: "843095790410",
   appId: "1:843095790410:web:2649cdc260da53158ae7b3",
   measurementId: "G-WRRJ35JL61"
@@ -16,7 +16,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// anonim giriş (gizli, kullanıcının müdahalesine gerek yok)
+// anonim kullanıcı oturumu aç
 signInAnonymously(auth).catch((error) => {
   console.error('Firebase Auth Error:', error);
 });
