fb246597a4240d44ee7162a789faeaf1f3947e97
whiteeagle01 <turkozumustafa@gmail.com>
2025-06-08 15:02:47 +0300

    init

---
+++ api/frame-image.tsx
@@ -0,0 +1,32 @@
+// api/frame-image.tsx
+import { ImageResponse } from '@vercel/og';
+import React from 'react';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default function handler() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          fontSize: 40,
+          background: 'black',
+          color: 'white',
+          width: '100%',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Vote for the next crypto champion 🥇
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}
